4ff56dc7adebfca90b0d226ba4c41d52c812754b
Misha <Lampard0307@yandex.ru>
2019-01-15 00:59:50 +0300

    initial commit

---
+++ src/EditorIllustration.java
@@ -0,0 +1,36 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.SelectionModel;
+import com.intellij.openapi.project.Project;
+
+public class EditorIllustration extends AnAction {
+    @Override
+    public void update(AnActionEvent e){
+        final Project project = e.getProject();
+        final Editor editor = e.getData(CommonDataKeys.EDITOR);
+        e.getPresentation().setVisible(project != null && editor != null &&
+            editor.getSelectionModel().hasSelection());
+    }
+
+    @Override
+    public void actionPerformed(final AnActionEvent e){
+        System.out.println("action is coming");
+        final Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        final Project project = e.getProject();
+
+        final Document document = editor.getDocument();
+        final SelectionModel selectionModel = editor.getSelectionModel();
+
+        final int start = selectionModel.getSelectionStart();
+        final int end = selectionModel.getSelectionEnd();
+
+        WriteCommandAction.runWriteCommandAction(project, () ->
+                document.replaceString(start, end, "Replacement")
+        );
+        selectionModel.removeSelection();
+    }
+}
